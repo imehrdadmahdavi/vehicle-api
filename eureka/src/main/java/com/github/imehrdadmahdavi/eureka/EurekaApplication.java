272d33c6903e5d05841f4791b83d5fd78bf95b55
Mehrdad Mahdavi <imehrdadmahdavi@gmail.com>
2020-08-08 12:48:08 -0700

    Refactor

--- eureka/src/main/java/com/github/imehrdadmahdavi/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/github/imehrdadmahdavi/eureka/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.udacity.eureka;
+package com.github.imehrdadmahdavi.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
